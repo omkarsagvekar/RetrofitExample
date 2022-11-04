f04ea356446089adb80b446e291e1bc3fb7808b6
OMKAR <sagvekaromkar940@gmail.com>
2022-11-04 15:47:43 +0530

    First commit

---
+++ app/src/main/java/com/example/retrofitexample/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.retrofitexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvResult = findViewById(R.id.tv_result);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/").addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceholderApi jsonPlaceholderApi = retrofit.create(JsonPlaceholderApi.class);
+        Call<List<Post>> call = jsonPlaceholderApi.getPosts();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()){
+                    tvResult.setText("Code: "+response.code());
+                }
+                List<Post> posts = response.body();
+                for(Post post : posts){
+                    String content = "";
+                    content += "ID: " + post.getId() + "\n";
+                    content += "UserID: " + post.getUserId() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+                    content += "Text: " + post.getText() + "\n\n";
+
+                    tvResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                tvResult.setText(t.getMessage());
+
+            }
+        });
+    }
+}
